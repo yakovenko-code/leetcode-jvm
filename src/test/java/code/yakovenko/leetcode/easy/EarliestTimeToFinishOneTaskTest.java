d9a0d51acde6e431bb9faf5a04edd3da310038b7
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:17:52 +0300

    ADD: 3683. Earliest Time to Finish One Task

---
+++ src/test/java/code/yakovenko/leetcode/easy/EarliestTimeToFinishOneTaskTest.java
@@ -0,0 +1,20 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class EarliestTimeToFinishOneTaskTest {
+
+	private final EarliestTimeToFinishOneTask solution = new EarliestTimeToFinishOneTask();
+
+	@Test
+	public void test1() {
+		assertThat(solution.earliestTime(new int[][]{{1, 6}, {2, 3}})).isEqualTo(5);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.earliestTime(new int[][]{{100, 100}, {100, 100}, {100, 100}})).isEqualTo(200);
+	}
+}
