c95b58f4a4e65dd8976e79729dbd057e1f8b2ddb
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:36:14 +0300

    ADD: 3536. Maximum Product of Two Digits

---
+++ src/test/java/code/yakovenko/leetcode/easy/MaximumProductOfTwoDigitsTest.java
@@ -0,0 +1,25 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class MaximumProductOfTwoDigitsTest {
+
+	private final MaximumProductOfTwoDigits solution = new MaximumProductOfTwoDigits();
+
+	@Test
+	public void test1() {
+		assertThat(solution.maxProduct(31)).isEqualTo(3);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.maxProduct(22)).isEqualTo(4);
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.maxProduct(124)).isEqualTo(8);
+	}
+}
