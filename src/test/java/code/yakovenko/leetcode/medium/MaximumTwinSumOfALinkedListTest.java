2e57916884ee7971f55478d3fa83caa7cb4201cb
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:56:41 +0300

    ADD: 2130. Maximum Twin Sum of a Linked List

---
+++ src/test/java/code/yakovenko/leetcode/medium/MaximumTwinSumOfALinkedListTest.java
@@ -0,0 +1,28 @@
+package code.yakovenko.leetcode.medium;
+
+import code.yakovenko.leetcode.utils.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class MaximumTwinSumOfALinkedListTest {
+
+	private final MaximumTwinSumOfALinkedList solution = new MaximumTwinSumOfALinkedList();
+
+	@Test
+	public void test1() {
+		assertThat(solution.pairSum(new ListNode(5, new ListNode(4, new ListNode(2, new ListNode(1))))))
+				.isEqualTo(6);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.pairSum(new ListNode(4, new ListNode(2, new ListNode(2, new ListNode(3))))))
+				.isEqualTo(7);
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.pairSum(new ListNode(1, new ListNode(100000)))).isEqualTo(100001);
+	}
+}
