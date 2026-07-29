99ccdf73ddbc3f69faea1f29337eef84895f48ba
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:55:47 +0300

    ADD: 206. Reverse Linked List

---
+++ src/test/java/code/yakovenko/leetcode/easy/ReverseLinkedListTest.java
@@ -0,0 +1,47 @@
+package code.yakovenko.leetcode.easy;
+
+import code.yakovenko.leetcode.Comparators;
+import code.yakovenko.leetcode.utils.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class ReverseLinkedListTest {
+
+	private final ReverseLinkedList solution = new ReverseLinkedList();
+
+	@Test
+	public void test1() {
+		assertThat(
+				solution.reverseList(
+						new ListNode(1,
+								new ListNode(2,
+										new ListNode(3,
+												new ListNode(4,
+														new ListNode(5)))))
+				)
+		).usingComparator(
+				Comparators::listNode
+		).isEqualTo(
+				new ListNode(
+						5, new ListNode(
+						4, new ListNode(
+						3, new ListNode(
+						2, new ListNode(1)))))
+		);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.reverseList(new ListNode(1, new ListNode(2))))
+				.usingComparator(Comparators::listNode)
+				.isEqualTo(new ListNode(2, new ListNode(1)));
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.reverseList(null))
+				.usingComparator(Comparators::listNode)
+				.isEqualTo(null);
+	}
+}
