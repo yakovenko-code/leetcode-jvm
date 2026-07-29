06422735a9c44954b6e88e2c148efcfcdb682aa8
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:24:46 +0300

    ADD: 3921. Score Validator

---
+++ src/main/java/code/yakovenko/leetcode/easy/ScoreValidator.java
@@ -0,0 +1,27 @@
+package code.yakovenko.leetcode.easy;
+
+public final class ScoreValidator {
+
+	public int[] scoreValidator(String[] events) {
+		int score = 0, counter = 0;
+
+		for (int i = 0; i < events.length && counter != 10; i++) {
+			final String event = events[i];
+			final char firstChar = event.charAt(0);
+
+			if (firstChar == 'W') {
+				if (event.length() == 1) {
+					counter++;
+				} else {
+					score++;
+				}
+			} else if (firstChar == 'N') {
+				score++;
+			} else {
+				score += firstChar - '0';
+			}
+		}
+
+		return new int[]{score, counter};
+	}
+}
---
+++ src/test/java/code/yakovenko/leetcode/easy/ScoreValidatorTest.java
@@ -0,0 +1,26 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class ScoreValidatorTest {
+
+	private final ScoreValidator solution = new ScoreValidator();
+
+	@Test
+	public void test1() {
+		assertThat(solution.scoreValidator(new String[]{"1", "4", "W", "6", "WD"})).isEqualTo(new int[]{12, 1});
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.scoreValidator(new String[]{"WD", "NB", "0", "4", "4"})).isEqualTo(new int[]{10, 0});
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.scoreValidator(new String[]{"W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W"}))
+				.isEqualTo(new int[]{0, 10});
+	}
+}
