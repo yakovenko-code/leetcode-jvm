a65597338a10d44288250da7174294395ffb0d67
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:39:29 +0300

    ADD: 3248. Snake in Matrix

---
+++ src/main/java/code/yakovenko/leetcode/easy/SnakeInMatrix.java
@@ -0,0 +1,21 @@
+package code.yakovenko.leetcode.easy;
+
+import java.util.List;
+
+public final class SnakeInMatrix {
+
+	public int finalPositionOfSnake(int n, List<String> commands) {
+		int position = 0;
+
+		for (final String command : commands) {
+			switch (command.charAt(0)) {
+				case 'U' -> position -= n;
+				case 'R' -> position++;
+				case 'D' -> position += n;
+				case 'L' -> position--;
+			}
+		}
+
+		return position;
+	}
+}
