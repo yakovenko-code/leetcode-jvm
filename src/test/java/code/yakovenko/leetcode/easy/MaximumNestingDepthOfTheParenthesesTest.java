a808747f8c3339f64b5eb22e3710cdf280b3d1ae
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 19:01:45 +0300

    ADD: 1614. Maximum Nesting Depth of the Parentheses

---
+++ src/test/java/code/yakovenko/leetcode/easy/MaximumNestingDepthOfTheParenthesesTest.java
@@ -0,0 +1,25 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class MaximumNestingDepthOfTheParenthesesTest {
+
+	private final MaximumNestingDepthOfTheParentheses solution = new MaximumNestingDepthOfTheParentheses();
+
+	@Test
+	public void test1() {
+		assertThat(solution.maxDepth("(1+(2*3)+((8)/4))+1")).isEqualTo(3);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.maxDepth("(1)+((2))+(((3)))")).isEqualTo(3);
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.maxDepth("()(())((()()))")).isEqualTo(3);
+	}
+}
