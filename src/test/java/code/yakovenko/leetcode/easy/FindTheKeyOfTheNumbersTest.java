e6a280acc7d0b1702c9fbe107225add22390b168
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:57:52 +0300

    ADD: 3270. Find the Key of the Numbers

---
+++ src/test/java/code/yakovenko/leetcode/easy/FindTheKeyOfTheNumbersTest.java
@@ -0,0 +1,25 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class FindTheKeyOfTheNumbersTest {
+
+	private final FindTheKeyOfTheNumbers solution = new FindTheKeyOfTheNumbers();
+
+	@Test
+	public void test1() {
+		assertThat(solution.generateKey(1, 10, 1000)).isEqualTo(0);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.generateKey(987, 879, 798)).isEqualTo(777);
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.generateKey(1, 2, 3)).isEqualTo(1);
+	}
+}
