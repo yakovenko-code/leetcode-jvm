ebd453d32e58be1d13aec273023b26e860021558
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-29 17:15:39 +0300

    ADD: 1313. Decompress Run-Length Encoded List

---
+++ src/main/java/code/yakovenko/leetcode/easy/DecompressRunLengthEncodedList.java
@@ -0,0 +1,27 @@
+package code.yakovenko.leetcode.easy;
+
+import java.util.Arrays;
+
+public final class DecompressRunLengthEncodedList {
+
+	public int[] decompressRLElist(int[] nums) {
+		int size = 0;
+
+		for (int i = 0; i < nums.length; i += 2) {
+			size += nums[i];
+		}
+
+		final int[] result = new int[size];
+		int position = 0;
+
+		for (int i = 0; i < nums.length; i += 2) {
+			final int[] tmp = new int[nums[i]];
+
+			Arrays.fill(tmp, nums[i + 1]);
+			System.arraycopy(tmp, 0, result, position, tmp.length);
+			position += tmp.length;
+		}
+
+		return result;
+	}
+}
