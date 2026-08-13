6a401500dd86f747bfaa82d3b22f4e8c5ccc4897
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 14:47:13 +0300

    ADD: 191. Number of 1 Bits

---
+++ src/test/java/code/yakovenko/leetcode/easy/NumberOf1BitsTest.java
@@ -0,0 +1,25 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class NumberOf1BitsTest {
+
+	private final NumberOf1Bits solution = new NumberOf1Bits();
+
+	@Test
+	public void test1() {
+		assertThat(solution.hammingWeight(11)).isEqualTo(3);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.hammingWeight(128)).isEqualTo(1);
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.hammingWeight(2147483645)).isEqualTo(30);
+	}
+}
