0abfff5d86f54702a496ee9d12422b8127b0f4f0
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 15:13:24 +0300

    ADD: 3856. Trim Trailing Vowels

---
+++ src/main/java/code/yakovenko/leetcode/easy/TrimTrailingVowels.java
@@ -0,0 +1,23 @@
+package code.yakovenko.leetcode.easy;
+
+public final class TrimTrailingVowels {
+
+	private static final boolean[] IS_VOWEL = new boolean[256];
+
+	static {
+		for (final char vowel : "aeiou".toCharArray()) {
+			IS_VOWEL[vowel] = true;
+		}
+	}
+
+	public String trimTrailingVowels(String s) {
+		int right = s.length();
+
+
+		while (right > 0 && IS_VOWEL[s.charAt(right - 1)]) {
+			right--;
+		}
+
+		return s.substring(0, right);
+	}
+}
