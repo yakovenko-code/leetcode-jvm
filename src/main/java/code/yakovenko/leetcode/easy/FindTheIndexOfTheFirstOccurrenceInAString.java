fb41a885a85d53578256fa350a4c53c4de31d56b
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 20:31:44 +0300

    ADD: 28. Find the Index of the First Occurrence in a String

---
+++ src/main/java/code/yakovenko/leetcode/easy/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,8 @@
+package code.yakovenko.leetcode.easy;
+
+public final class FindTheIndexOfTheFirstOccurrenceInAString {
+
+	public int strStr(String haystack, String needle) {
+		return haystack.indexOf(needle);
+	}
+}
