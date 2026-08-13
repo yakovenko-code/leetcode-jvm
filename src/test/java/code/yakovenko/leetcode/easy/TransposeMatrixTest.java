6765c7b6762938caa332ba7dede860eaa83bd5e3
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 16:21:15 +0300

    ADD: 867. Transpose Matrix

---
+++ src/test/java/code/yakovenko/leetcode/easy/TransposeMatrixTest.java
@@ -0,0 +1,22 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class TransposeMatrixTest {
+
+	private final TransposeMatrix solution = new TransposeMatrix();
+
+	@Test
+	public void test1() {
+		assertThat(solution.transpose(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}))
+				.isEqualTo(new int[][]{{1, 4, 7}, {2, 5, 8}, {3, 6, 9}});
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.transpose(new int[][]{{1, 2, 3}, {4, 5, 6}}))
+				.isEqualTo(new int[][]{{1, 4}, {2, 5}, {3, 6}});
+	}
+}
