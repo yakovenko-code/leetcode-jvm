c234a420e43f895968328c7aafd56447bcd66a92
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 16:43:30 +0300

    ADD: 2255. Count Prefixes of a Given String

---
+++ src/test/java/code/yakovenko/leetcode/easy/CountPrefixesOfAGivenStringTest.java
@@ -0,0 +1,21 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class CountPrefixesOfAGivenStringTest {
+
+	private final CountPrefixesOfAGivenString solution = new CountPrefixesOfAGivenString();
+
+	@Test
+	public void test1() {
+		assertThat(solution.countPrefixes(new String[]{"a", "b", "c", "ab", "bc", "abc"}, "abc"))
+				.isEqualTo(3);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.countPrefixes(new String[]{"a", "a"}, "aa")).isEqualTo(2);
+	}
+}
