ffd6c6e16fe152e2758379dd1e801f4c23240cde
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 15:00:47 +0300

    ADD: 1598. Crawler Log Folder

---
+++ src/test/java/code/yakovenko/leetcode/easy/CrawlerLogFolderTest.java
@@ -0,0 +1,25 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class CrawlerLogFolderTest {
+
+	private final CrawlerLogFolder solution = new CrawlerLogFolder();
+
+	@Test
+	public void test1() {
+		assertThat(solution.minOperations(new String[]{"d1/", "d2/", "../", "d21/", "./"})).isEqualTo(2);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.minOperations(new String[]{"d1/", "d2/", "./", "d3/", "../", "d31/"})).isEqualTo(3);
+	}
+
+	@Test
+	public void test3() {
+		assertThat(solution.minOperations(new String[]{"d1/", "../", "../", "../"})).isEqualTo(0);
+	}
+}
