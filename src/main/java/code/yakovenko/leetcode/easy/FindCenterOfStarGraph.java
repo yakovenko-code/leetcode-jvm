6b92f943b14658e20af477db73b9f6ed28a9fa64
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 14:53:29 +0300

    ADD: 1791. Find Center of Star Graph

---
+++ src/main/java/code/yakovenko/leetcode/easy/FindCenterOfStarGraph.java
@@ -0,0 +1,12 @@
+package code.yakovenko.leetcode.easy;
+
+public final class FindCenterOfStarGraph {
+
+	public int findCenter(int[][] edges) {
+		if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+			return edges[0][0];
+		} else {
+			return edges[0][1];
+		}
+	}
+}
---
+++ src/test/java/code/yakovenko/leetcode/easy/FindCenterOfStarGraphTest.java
@@ -0,0 +1,20 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class FindCenterOfStarGraphTest {
+
+	private final FindCenterOfStarGraph solution = new FindCenterOfStarGraph();
+
+	@Test
+	public void test1() {
+		assertThat(solution.findCenter(new int[][]{{1, 2}, {2, 3}, {4, 2}})).isEqualTo(2);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.findCenter(new int[][]{{1, 2}, {5, 1}, {1, 3}, {1, 4}})).isEqualTo(1);
+	}
+}
