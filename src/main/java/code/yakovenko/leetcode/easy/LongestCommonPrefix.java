efbf3627e46b8059f6be6a7d838735c7bb62fd8f
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 20:30:29 +0300

    ADD: 14. Longest Common Prefix

---
+++ src/main/java/code/yakovenko/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package code.yakovenko.leetcode.easy;
+
+public final class LongestCommonPrefix {
+
+	public String longestCommonPrefix(String[] strs) {
+		String prefix = strs[0];
+
+		for (int i = 1; i < strs.length; i++) {
+			final String str = strs[i];
+
+			while (!str.startsWith(prefix)) {
+				prefix = prefix.substring(0, prefix.length() - 1);
+			}
+
+			if (prefix.isEmpty()) {
+				break;
+			}
+		}
+
+		return prefix;
+	}
+}
