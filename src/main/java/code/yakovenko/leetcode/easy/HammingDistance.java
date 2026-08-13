e0254a732bfe9af5825ba049facda12f096fc863
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 14:46:33 +0300

    ADD: 461. Hamming Distance

---
+++ src/main/java/code/yakovenko/leetcode/easy/HammingDistance.java
@@ -0,0 +1,8 @@
+package code.yakovenko.leetcode.easy;
+
+public final class HammingDistance {
+
+	public int hammingDistance(int x, int y) {
+		return Integer.bitCount(x ^ y);
+	}
+}
---
+++ src/test/java/code/yakovenko/leetcode/easy/HammingDistanceTest.java
@@ -0,0 +1,20 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class HammingDistanceTest {
+
+	private final HammingDistance solution = new HammingDistance();
+
+	@Test
+	public void test1() {
+		assertThat(solution.hammingDistance(1, 4)).isEqualTo(2);
+	}
+
+	@Test
+	public void test2() {
+		assertThat(solution.hammingDistance(3, 1)).isEqualTo(1);
+	}
+}
