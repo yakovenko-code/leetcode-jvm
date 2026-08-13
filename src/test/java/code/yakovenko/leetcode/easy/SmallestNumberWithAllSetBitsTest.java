70b9610c14d8a8a0004339a3ca97b76f398a2618
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-08-13 20:32:48 +0300

    FIX: 3370. Smallest Number With All Set Bits

--- src/test/java/code/yakovenko/leetcode/easy/SmallestNumberWithAllSetBitsTest.java
+++ src/test/java/code/yakovenko/leetcode/easy/SmallestNumberWithAllSetBitsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SmallestNumberWithAllSetBitsTest {
+public final class SmallestNumberWithAllSetBitsTest {
 
 	private final SmallestNumberWithAllSetBits solution = new SmallestNumberWithAllSetBits();
 
