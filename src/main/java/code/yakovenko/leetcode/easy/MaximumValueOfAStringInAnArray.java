4cb13feda5dab149e96eb721c6118134d141992c
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-02 13:38:35 +0300

    ADD: 2496. Maximum Value of a String in an Array

---
+++ src/main/java/code/yakovenko/leetcode/easy/MaximumValueOfAStringInAnArray.java
@@ -0,0 +1,23 @@
+package code.yakovenko.leetcode.easy;
+
+public final class MaximumValueOfAStringInAnArray {
+
+	public int maximumValue(String[] strs) {
+		int max = 0;
+
+		for (String str : strs) {
+			boolean isNumeric = true;
+
+			for (int i = 0; i < str.length(); i++) {
+				if (!Character.isDigit(str.charAt(i))) {
+					isNumeric = false;
+					break;
+				}
+			}
+
+			max = Math.max(max, isNumeric ? Integer.parseInt(str) : str.length());
+		}
+
+		return max;
+	}
+}
