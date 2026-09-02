674de7ea4219a8a59556693f50698b76869b95c7
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-02 13:37:01 +0300

    ADD: 3274. Check if Two Chessboard Squares Have the Same Color

---
+++ src/main/java/code/yakovenko/leetcode/easy/CheckIfTwoChessboardSquaresHaveTheSameColor.java
@@ -0,0 +1,11 @@
+package code.yakovenko.leetcode.easy;
+
+public final class CheckIfTwoChessboardSquaresHaveTheSameColor {
+
+	public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
+		final int color1 = ((coordinate1.charAt(0) - 'a' + 1) + (coordinate1.charAt(1) - '0')) % 2;
+		final int color2 = ((coordinate2.charAt(0) - 'a' + 1) + (coordinate2.charAt(1) - '0')) % 2;
+
+		return color1 == color2;
+	}
+}
