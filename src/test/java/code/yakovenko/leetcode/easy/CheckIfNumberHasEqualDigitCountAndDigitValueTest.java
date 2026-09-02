048d52156438a5b41d8bb8df2ff13f0b45b28ab5
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-02 13:01:49 +0300

    ADD: 2283. Check if Number Has Equal Digit Count and Digit Value

---
+++ src/test/java/code/yakovenko/leetcode/easy/CheckIfNumberHasEqualDigitCountAndDigitValueTest.java
@@ -0,0 +1,21 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class CheckIfNumberHasEqualDigitCountAndDigitValueTest {
+
+	private final CheckIfNumberHasEqualDigitCountAndDigitValue solution =
+			new CheckIfNumberHasEqualDigitCountAndDigitValue();
+
+	@Test
+	public void example1() {
+		assertThat(solution.digitCount("1210")).isTrue();
+	}
+
+	@Test
+	public void example2() {
+		assertThat(solution.digitCount("030")).isFalse();
+	}
+}
