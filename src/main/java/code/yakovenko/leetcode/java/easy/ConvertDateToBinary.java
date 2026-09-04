57bec4b4bf5e7e16857daa6a93a1cc602a6dcdf1
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-04 17:01:45 +0300

    FIX: 3280. Convert Date to Binary

--- src/main/java/code/yakovenko/leetcode/java/easy/ConvertDateToBinary.java
+++ src/main/java/code/yakovenko/leetcode/java/easy/ConvertDateToBinary.java
@@ -3,13 +3,14 @@ package code.yakovenko.leetcode.java.easy;
 public final class ConvertDateToBinary {
 
 	public String convertDateToBinary(String date) {
+		final StringBuilder stringBuilder = new StringBuilder();
 
-		String stringBuilder = Integer.toBinaryString(Integer.parseInt(date.substring(0, 4))) +
-				'-' +
-				Integer.toBinaryString(Integer.parseInt(date.substring(5, 7))) +
-				'-' +
-				Integer.toBinaryString(Integer.parseInt(date.substring(8, 10)));
+		stringBuilder.append(Integer.toBinaryString(Integer.parseInt(date.substring(0, 4))));
+		stringBuilder.append('-');
+		stringBuilder.append(Integer.toBinaryString(Integer.parseInt(date.substring(5, 7))));
+		stringBuilder.append('-');
+		stringBuilder.append(Integer.toBinaryString(Integer.parseInt(date.substring(8, 10))));
 
-		return stringBuilder;
+		return stringBuilder.toString();
 	}
 }
