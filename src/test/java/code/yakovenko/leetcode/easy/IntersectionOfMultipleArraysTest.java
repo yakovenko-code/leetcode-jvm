3f1acc67c55226b24f18c9f87be6d481e92fa090
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-04 15:59:13 +0300

    ADD: 2248. Intersection of Multiple Arrays

---
+++ src/test/java/code/yakovenko/leetcode/easy/IntersectionOfMultipleArraysTest.java
@@ -0,0 +1,24 @@
+package code.yakovenko.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class IntersectionOfMultipleArraysTest {
+
+	private final IntersectionOfMultipleArrays solution = new IntersectionOfMultipleArrays();
+
+	@Test
+	public void example1() {
+		assertThat(solution.intersection(new int[][]{{3, 1, 2, 4, 5}, {1, 2, 3, 4}, {3, 4, 5, 6}}))
+				.isEqualTo(List.of(3, 4));
+	}
+
+	@Test
+	public void example2() {
+		assertThat(solution.intersection(new int[][]{{1, 2, 3}, {4, 5, 6}}))
+				.isEqualTo(List.of());
+	}
+}
