1647122f4b07cc34da57dafe2ecfd6da2a8c2d2a
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-21 14:23:57 +0300

    ADD: 3131. Find the Integer Added to Array I

---
+++ src/test/java/code/yakovenko/leetcode/java/easy/FindTheIntegerAddedToArrayITest.java
@@ -0,0 +1,25 @@
+package code.yakovenko.leetcode.java.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class FindTheIntegerAddedToArrayITest {
+
+	private final FindTheIntegerAddedToArrayI solution = new FindTheIntegerAddedToArrayI();
+
+	@Test
+	public void example1() {
+		assertThat(solution.addedInteger(new int[]{2, 6, 4}, new int[]{9, 7, 5})).isEqualTo(3);
+	}
+
+	@Test
+	public void example2() {
+		assertThat(solution.addedInteger(new int[]{10}, new int[]{5})).isEqualTo(-5);
+	}
+
+	@Test
+	public void example3() {
+		assertThat(solution.addedInteger(new int[]{1, 1, 1, 1}, new int[]{1, 1, 1, 1})).isEqualTo(0);
+	}
+}
