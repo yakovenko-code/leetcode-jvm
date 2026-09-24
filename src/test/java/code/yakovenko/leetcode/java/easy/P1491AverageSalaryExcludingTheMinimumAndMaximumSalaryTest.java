a0c8ddea238a8a07118621e99ab401d9508d0f9b
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-24 12:38:23 +0300

    FIX: 1491. Average Salary Excluding the Minimum and Maximum Salary

--- src/test/java/code/yakovenko/leetcode/java/easy/P1491AverageSalaryExcludingTheMinimumAndMaximumSalaryTest.java
+++ src/test/java/code/yakovenko/leetcode/java/easy/P1491AverageSalaryExcludingTheMinimumAndMaximumSalaryTest.java
@@ -1,5 +1,6 @@
 package code.yakovenko.leetcode.java.easy;
 
+import code.yakovenko.leetcode.java.Constants;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,11 +12,13 @@ public final class P1491AverageSalaryExcludingTheMinimumAndMaximumSalaryTest {
 
 	@Test
 	public void example1() {
-		assertThat(solution.average(new int[]{4000, 3000, 1000, 2000})).isEqualTo(2500.00000);
+		assertThat(solution.average(new int[]{4000, 3000, 1000, 2000}))
+				.isCloseTo(2500.00000, Constants.TOLERANCE);
 	}
 
 	@Test
 	public void example2() {
-		assertThat(solution.average(new int[]{1000, 2000, 3000})).isEqualTo(2000.00000);
+		assertThat(solution.average(new int[]{1000, 2000, 3000}))
+				.isCloseTo(2000.00000, Constants.TOLERANCE);
 	}
 }
