8e581716c879caf09d95820513b4005e6c01fee9
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-09-24 12:37:51 +0300

    FIX: Comparators

--- src/test/java/code/yakovenko/leetcode/java/Comparators.java
+++ src/test/java/code/yakovenko/leetcode/java/Comparators.java
@@ -4,6 +4,10 @@ import code.yakovenko.leetcode.java.utils.ListNode;
 
 public final class Comparators {
 
+	private Comparators() {
+		throw new AssertionError("No Comparators instances for you!");
+	}
+
 	public static int listNode(ListNode list1, ListNode list2) {
 		if (list1 == null && list2 == null) return 0;
 		if (list1 == null) return -1;
